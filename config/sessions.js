4ec8f6c7f26055abb2eeeae641689b26962129d6
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-05-07 17:39:25 +0200

    passport and sessions files

---
+++ config/sessions.js
@@ -0,0 +1,31 @@
+require("./config/mongoose-config");
+
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+
+//Passport
+const session = require('express-session');
+const passport = require('passport');
+
+require('./config/passport');
+
+// ADD SESSION SETTINGS HERE:
+app.use(session({
+  secret: "some secret goes here",
+  resave: true,
+  saveUninitialized: true
+}));
+
+// USE passport.initialize() and passport.session() HERE:
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ROUTES MIDDLEWARE STARTS HERE:
+
+const authRoutes = require('./auth/auth-routes');
+app.use('/api', authRoutes);
+
+
+module.exports = app;
