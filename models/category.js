bacc9bc17da20594dd984579ab02b9cb80e707b0
1testsample <frankomarmier@gmail.com>
2019-05-11 14:20:01 +0200

    eyyy

--- models/category.js
+++ models/category.js
@@ -4,13 +4,14 @@ const Schema = mongoose.Schema;
 const categorySchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
   tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }]
 });
 
-categorySchema.index({ name: 1 }, { unique: true });
+categorySchema.index({ name: 2 }, { unique: true });
 const categoryModel = mongoose.model("Category", categorySchema);
 
 module.exports = categoryModel;
